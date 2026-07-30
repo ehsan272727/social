3c41e447e50a0c62f450aaccdc36327de138c42a
ehsan <ehsansf2727@gmail.com>
2026-07-30 17:30:12 +0330

    Install better-auth

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "./src/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "./src/prisma/migrations",
   },
   datasource: {
     url: env("DATABASE_URL"),
