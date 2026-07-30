f189fc7ae01d3565cf858c9cf7f5136370577527
ehsan <ehsansf2727@gmail.com>
2026-07-30 20:59:32 +0330

    add google auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,18 @@ import prisma from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [nextCookies()], // make sure NextCookies is the last plugin in the array
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
